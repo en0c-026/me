aa25c4968962f90b6ca1ae6aa26b0466a139ad15
Samsung <bitup.games@gmail.com>
2023-03-29 17:17:40 +0200

    Updates

--- _next/static/cpzv88gkqYdhU0nbp14py/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-e7625c3a782d4635.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
